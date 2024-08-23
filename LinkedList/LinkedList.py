739ffba20c51278bf77065cc4c45d77c5e5e1fef
Pranavkangale <kangalepranav@gmail.com>
2024-08-24 01:11:08 +0530

    update LinkedList.py

--- LinkedList/LinkedList.py
+++ LinkedList/LinkedList.py
@@ -7,14 +7,19 @@ class Node:  # creates a new node
         self.next = None
 
 
-class LinkeList:
+class LinkedList:
     def __init__(self, value):
         new_node = Node(value)  # calls the Node Class and passes the value
         self.head = new_node
         self.tail = new_node
         self.length = new_node
 
+    def print_ll_List(self):  # method to print the Linked list
+        temp = self.head
+        while temp is not None:
+            print(temp.value)
+            temp = temp.next
 
-my_Linkedlist = LinkeList(4)
 
-print(my_Linkedlist.head.value)
+my_linked_list = LinkedList(8)
+my_linked_list.print_ll_List()
