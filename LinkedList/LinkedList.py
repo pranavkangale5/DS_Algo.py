cf5b27c30aab64fe8737801ae21689146245a6c9
Pranavkangale <kangalepranav@gmail.com>
2024-08-24 02:42:41 +0530

    update LinkedList.py

--- LinkedList/LinkedList.py
+++ LinkedList/LinkedList.py
@@ -12,7 +12,7 @@ class LinkedList:
         new_node = Node(value)  # calls the Node Class and passes the value
         self.head = new_node
         self.tail = new_node
-        self.length = new_node
+        self.length = 1
 
     def print_ll_List(self):  # method to print the Linked list
         temp = self.head
@@ -20,6 +20,20 @@ class LinkedList:
             print(temp.value)
             temp = temp.next
 
+    def append(self, value):  # method to append a node to Lniked List
+        new_node = Node(value)  # calls the Node Class and passes the value
+        if self.length == 0:  # checks if the linked list is empty
+            self.head = new_node
+            self.tail = new_node
+        else:
+            self.tail.next = new_node  # points the next of the last node
+            self.tail = new_node  # updates the tail to the new node
+        self.length += 1
+        return True
+
 
 my_linked_list = LinkedList(8)
+my_linked_list.print_ll_List()
+my_linked_list.append(3)
+my_linked_list.append(23)
 my_linked_list.print_ll_List()
